453eee8ed162f5127c2ea3031052aa61c8c089de
Matheus Carvalho <matheus.contatoprofissional.engc@gmail.com>
2022-11-21 00:31:14 -0300

    Commiting the first project

--- BandNameGenerator/main.py
+++ BandNameGenerator/main.py
@@ -1,1 +1,9 @@
-# main
+# 1- Greetings.
+print("Welcome to the Band Name Generator.")
+
+# 2- Inputs.
+city = input("What city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+# 3- Concatenate the strings and print it out.
+print(f"Your band's name should be: {city} {pet}.")
